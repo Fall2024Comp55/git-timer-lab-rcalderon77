f1ab64268e7728ef118eea9ffb2eada83a830227
Rogelio <r_calderon3@u.pacific.edu>
2024-10-06 21:08:05 -0700

    If i were to use pause, it would look choppy and not smooth as it would pause everything.

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -27,6 +27,12 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void mousePressed(MouseEvent e) {
+		for (GOval ball : balls) {
+	        if (ball.getX() < 100) {
+	            return;
+	        }
+	    }
+		
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
 	}
