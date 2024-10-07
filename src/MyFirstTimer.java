5783fc43f1be47bf343a12c4ba2a21bb415b8a1d
Rogelio <r_calderon3@u.pacific.edu>
2024-10-06 20:19:12 -0700

    A timer needs how long is each interval, the object, and the action it's performing.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,12 +1,19 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import acm.graphics.GLabel;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class MyFirstTimer extends GraphicsProgram {
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
-
+	private int numTimes = 0;
+    private Timer t;
+	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
@@ -15,9 +22,26 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+
+        t = new Timer(1000, this);
+        t.setInitialDelay(3000);
+        t.start();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		numTimes = numTimes + 1;		
+		myLabel.move(5,0);
+		myLabel.setLabel("times called? " + numTimes);
+		
+		if(numTimes == 10) {
+			t.stop();
+		}
+		
 	}
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
+		
 	}
 }
